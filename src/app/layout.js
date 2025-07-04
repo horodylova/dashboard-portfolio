36973cd8b304b51182134188182c713c9257d967
Svitlana Horodylova <horodylova.sv@gmail.com>
2025-07-04 17:09:36 +0100

    added metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,28 @@ import { Metadata } from "next";
 export const metadata = {
   title: "Svitlana Horodylova",
   description: "Portfolio of dashboard projects",
+  openGraph: {
+    title: "Svitlana Horodylova",
+    description: "Portfolio of dashboard projects",
+    images: [
+      {
+        url: "/photo-1.png",
+        width: 1200,
+        height: 630,
+        alt: "Svitlana Horodylova"
+      }
+    ],
+    locale: "en_US",
+    type: "website",
+    siteName: "Svitlana Horodylova Portfolio"
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Svitlana Horodylova",
+    description: "Portfolio of dashboard projects",
+    images: ["/photo-1.png"],
+    creator: "@svitlanahorodylova"
+  }
 };
 
 export default function RootLayout({ children }) {
